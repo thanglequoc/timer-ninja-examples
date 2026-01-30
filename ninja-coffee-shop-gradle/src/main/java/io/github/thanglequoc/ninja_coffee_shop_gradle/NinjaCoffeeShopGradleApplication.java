1d0bbcca74707942b571243729be8509a99fded9
Thang Le Quoc <thanglequoc.it@gmail.com>
2026-01-30 20:28:25 +0700

    feat: Add TimerNinjaTracker upon program start up

--- ninja-coffee-shop-gradle/src/main/java/io/github/thanglequoc/ninja_coffee_shop_gradle/NinjaCoffeeShopGradleApplication.java
+++ ninja-coffee-shop-gradle/src/main/java/io/github/thanglequoc/ninja_coffee_shop_gradle/NinjaCoffeeShopGradleApplication.java
@@ -3,11 +3,30 @@ package io.github.thanglequoc.ninja_coffee_shop_gradle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+
+import io.github.thanglequoc.ninja_coffee_shop_gradle.service.ShopKeepingService;
+import io.github.thanglequoc.timerninja.TimerNinjaTracker;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+
 @SpringBootApplication
 public class NinjaCoffeeShopGradleApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(NinjaCoffeeShopGradleApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(NinjaCoffeeShopGradleApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	@TimerNinjaTracker
+	public void openTheCoffeeShop() {
+		logger.info("Cleaning the coffee shop...");
+		ShopKeepingService shopKeepingService = new ShopKeepingService();
+		shopKeepingService.cleanTheShop();
+		logger.info("Opening the door. The coffee shop is now open!");
+	}
 }
---
+++ ninja-coffee-shop-gradle/src/main/java/io/github/thanglequoc/ninja_coffee_shop_gradle/service/ShopKeepingService.java
@@ -0,0 +1,45 @@
+package io.github.thanglequoc.ninja_coffee_shop_gradle.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.thanglequoc.timerninja.TimerNinjaTracker;
+
+public class ShopKeepingService {
+
+    // General CoffeeShop Keeping service
+
+    private Logger LOGGER = LoggerFactory.getLogger(ShopKeepingService.class);
+
+    @TimerNinjaTracker
+    public void cleanTheShop() {
+        mopTheFloor();
+        takeOutTheTrash();
+    }
+
+    @TimerNinjaTracker
+    private void mopTheFloor() {
+        try {
+            // Simulate cleaning process
+            Thread.sleep(500);
+            LOGGER.info("The shop has been mopped");
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @TimerNinjaTracker
+    private boolean takeOutTheTrash() {
+        try {
+            // Simulate trash taking process
+            Thread.sleep(200);
+            LOGGER.info("The trash has been taken out");
+            return true;
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return false;
+        }
+    }
+}
