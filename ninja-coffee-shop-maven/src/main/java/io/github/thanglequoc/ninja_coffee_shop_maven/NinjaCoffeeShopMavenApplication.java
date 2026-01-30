031e82fa7ff83f3b9069a974551486b20164a80a
Thang Le Quoc <thanglequoc.it@gmail.com>
2026-01-30 20:31:51 +0700

    feat: Add cleaning service on Maven

--- ninja-coffee-shop-maven/src/main/java/io/github/thanglequoc/ninja_coffee_shop_maven/NinjaCoffeeShopMavenApplication.java
+++ ninja-coffee-shop-maven/src/main/java/io/github/thanglequoc/ninja_coffee_shop_maven/NinjaCoffeeShopMavenApplication.java
@@ -1,13 +1,30 @@
 package io.github.thanglequoc.ninja_coffee_shop_maven;
 
+import io.github.thanglequoc.ninja_coffee_shop_maven.service.ShopKeepingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import io.github.thanglequoc.timerninja.TimerNinjaTracker;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class NinjaCoffeeShopMavenApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(NinjaCoffeeShopMavenApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(NinjaCoffeeShopMavenApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	@TimerNinjaTracker
+	public void openTheCoffeeShop() {
+		logger.info("Cleaning the coffee shop...");
+		ShopKeepingService shopKeepingService = new ShopKeepingService();
+		shopKeepingService.cleanTheShop();
+		logger.info("Opening the door. The coffee shop is now open!");
 	}
 
 }
